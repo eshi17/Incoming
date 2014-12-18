ea368173b210c0b839f591e2e364b600eebca7a1
eshi17 <eshi17@punahou.edu>
2014-12-17 14:53:30 -1000

    Simplified Game
    Beginning a simplified version of the game where the enemy is a single
    colored line. Currently, I have one of these coming down.

--- IncomingDesktop/Incoming_/Incoming_.ino
+++ IncomingDesktop/Incoming_/Incoming_.ino
@@ -6,6 +6,8 @@
 
 //int color = random(8);
 
+boolean enemyIncoming = true;
+
 struct Point
 {
   int x;
@@ -13,10 +15,8 @@ struct Point
   int color;
 };
 
-Point p1 = {1, 7, (random(6)+1)};
-Point p2 = {3, 7, (random(6)+1)};
-Point p3 = {5, 7, (random(6)+1)};
-Point myPoints[3] = {p1,p2,p3};
+Point p1 = {0, 7, (random(6)+1)};
+Point enemyPoints[1] = {p1};
 
 void setup()
 {
@@ -28,19 +28,40 @@ void loop()              // run over and over again
 {
   /*
   Draw the damn boxes at the top with random colors for each one
+  Write code for enemy boxes coming down into game play
   Write code for cursor that only moves at bottom between four slots
   Write in slots for cursor to move around in
-  Write in code for "up", which pushes selected colors into 
+  Write in code for "up", which pushes selected colors into game field
+  Write code for game over
   */
-  DrawBox();
+  DrawEnemy();
+  UpdateEnemy();
   DisplaySlate();
+  delay(1000);
+  ClearSlate();
 }
 
-void DrawBox()
+void DrawEnemy()
 {
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 8; i++)
   {
-    DrawPx(myPoints[i].x,myPoints[i].y,myPoints[i].color);
-    DrawPx(myPoints[i].x+1,myPoints[i].y,myPoints[i].color);
+    DrawPx(enemyPoints[i].x,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+1,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+2,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+3,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+4,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+5,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+6,enemyPoints[i].y,enemyPoints[i].color);
+    DrawPx(enemyPoints[i].x+7,enemyPoints[i].y,enemyPoints[i].color);
   }
+}
+
+
+void UpdateEnemy()
+{
+    for (int i = 0; i < 8; i++)
+    {
+      if (enemyPoints[i].y>1)
+        enemyPoints[i].y--;
+    }
 }
